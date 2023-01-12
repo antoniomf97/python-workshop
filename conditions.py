0cf40b32a8eed9e41e2617f9b34d3920d31d1ae9
António Fernandes <antonio.mf97@gmail.com>
2023-01-12 20:02:40 +0000

    update

--- conditions.py
+++ conditions.py
@@ -2,13 +2,53 @@
 Python Conditions
 """
 
-a = True
-b = False
 
-if a == True:
-    print("A is True")
-else:
-    print("A is False")
+# x = 5 > 10
+
+# print("Is 5 bigger than 10?", x)
+
+# x = 'alpha' == 'alfa'
+
+# print("Is alpha equal to alfa?", x)
+
+# x1 = 5 >= 3
+# x2 = 4 < 2
+# x3 = x1 & x2
+# x4 = x1 | x2
+
+# print(x1)
+# print(x2)
+# print(x3)
+# print(x4)
+
+# a = True
+# b = False
+
+# if a is True:
+#     print("A is True")
+# else:
+#     print("A is False")
+
+
+# cond_a = True
+# cond_b = False
+
+# print("A & B", cond_a and cond_b)
+# print("A & !B", cond_a and not cond_b)
+# print("!A & B", not cond_a and cond_b)
+# print("!A & !B", not cond_a and not cond_b)
+
+# print("A & B", cond_a or cond_b)
+# print("A & !B", cond_a or not cond_b)
+# print("!A & B", not cond_a or cond_b)
+# print("!A & !B", not cond_a or not cond_b)
+
+# cond_a = True
+# cond_b = False
+# cond_c = True
+
+# if cond_a and not cond_b and cond_c:
+#     print("It's true")
 
 
 """
@@ -19,39 +59,40 @@ C: 10-15
 D:  0-10    
 """
 
-grade = -1
+# grade = -1
 
-if 0 <= grade < 10:
-    print("D")
-if 10 <= grade < 15:
-    print("C")
-if 15 <= grade < 18:
-    print("B")
-if 18 <= grade <= 20:
-    print("A")
+# if 0 <= grade < 10:
+#     print("D")
+# if 10 <= grade < 15:
+#     print("C")
+# if 15 <= grade < 18:
+#     print("B")
+# if 18 <= grade <= 20:
+#     print("A")
 
 
-if 0 <= grade < 10:
-    print("D")
-else:
-    if 10 <= grade < 15:
-        print("C")
-    else:
-        if 15 <= grade < 18:
-            print("B")
-        else:
-            if 18 <= grade <= 20:
-                print("A")
-            else:
-                print("Invalid Grade")
+# if 0 <= grade < 10:
+#     print("D")
+# else:
+#     if 10 <= grade < 15:
+#         print("C")
+#     else:
+#         if 15 <= grade < 18:
+#             print("B")
+#         else:
+#             if 18 <= grade <= 20:
+#                 print("A")
+#             else:
+#                 print("Invalid Grade")
 
-if 0 <= grade < 10:
-    print("D")
-elif 10 <= grade < 15:
-    print("C")
-elif 15 <= grade < 18:
-    print("B")
-elif 18 <= grade <= 20:
-    print("A")
-else:
-    print("Invalid Grade")
+
+# if 0 <= grade < 10:
+#     print("D")
+# elif 10 <= grade < 15:
+#     print("C")
+# elif 15 <= grade < 18:
+#     print("B")
+# elif 18 <= grade <= 20:
+#     print("A")
+# else:
+#     print("Invalid Grade")
