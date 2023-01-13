8390399628dd2eec33f2c6d6433d220729215822
MrToino <antonio.mf97@gmail.com>
2023-01-13 18:08:05 +0000

    removed venv

--- venv/lib/python3.10/site-packages/julia/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="session")
-def Main(julia):
-    """pytest fixture for providing a Julia `Main` name space."""
-    from julia import Main
-
-    return Main
