7ca2cb2181003bd50ce4e7fd6c4049cb1980f2fe
António Fernandes <antonio.mf97@gmail.com>
2023-01-14 01:44:35 +0000

    cenas

--- basics/mammals.py
+++ basics/mammals.py
@@ -3,53 +3,53 @@ Python Classes Example
 """
 
 
-class Mammal:
-    def __init__(self, order, family, genus, species) -> None:
-        self.order = order
-        self.family = family
-        self.genus = genus
-        self.species = species
+# class Mammal:
+#     def __init__(self, order, family, genus, species) -> None:
+#         self.order = order
+#         self.family = family
+#         self.genus = genus
+#         self.species = species
 
-    def __str__(self) -> str:
-        return "{} |> {} |> {} |> {}".format(
-            self.order, self.family, self.genus, self.species
-        )
-
-
-class Dog(Mammal):
-    def __init__(self, name) -> None:
-        super().__init__("carnivora", "canidae", "canis", "c. familiaris")
-        self.dogname = name
-
-    def sound(self):
-        print(self.dogname + ": woof")
+#     def __str__(self) -> str:
+#         return "{} |> {} |> {} |> {}".format(
+#             self.order, self.family, self.genus, self.species
+#         )
 
 
-class Cat(Mammal):
-    def __init__(self, name) -> None:
-        super().__init__("carnivora", "felidae", "felis", "f. catus")
-        self.catname = name
+# class Dog(Mammal):
+#     def __init__(self, name) -> None:
+#         super().__init__("carnivora", "canidae", "canis", "c. familiaris")
+#         self.dogname = name
 
-    def sound(self):
-        print(self.catname + ": miau")
-
-
-print()
-dog = Dog(name="Doge")
-print(dog)
-dog.sound()
-print()
-
-cat = Cat(name="Garfield")
-print(cat)
-cat.sound()
+#     def sound(self):
+#         print(self.dogname + ": woof")
 
 
-def get_animal_species(animal: Mammal):
-    print(animal.species)
+# class Cat(Mammal):
+#     def __init__(self, name) -> None:
+#         super().__init__("carnivora", "felidae", "felis", "f. catus")
+#         self.catname = name
+
+#     def sound(self):
+#         print(self.catname + ": miau")
 
 
-print()
-get_animal_species(dog)
-get_animal_species(cat)
-print()
+# print()
+# dog = Dog(name="Doge")
+# print(dog)
+# dog.sound()
+# print()
+
+# cat = Cat(name="Garfield")
+# print(cat)
+# cat.sound()
+
+
+# def get_animal_species(animal: Mammal):
+#     print(animal.species)
+
+
+# print()
+# get_animal_species(dog)
+# get_animal_species(cat)
+# print()
